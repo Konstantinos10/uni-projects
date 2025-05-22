91fa5a7fb3b06b772e27e3c641211aa4434dfe45
Leon <leonkontos34@gmail.com>
2025-05-22 22:45:32 +0300

    question updates

--- questions.py
+++ questions.py
@@ -46,11 +46,24 @@ chapters ={1: {"title": "Basic Concepts",
                 "questions": {
                     1: "Basic Comparisons",
                     2: "If Statement",
-                    3: "Logical Operators"
-                    #4: "If-else Statements",
-                    #5: "If-elif-else Statements"
+                    3: "Else Statement",
+                    4: "Elif Statement",
+                    5: "Logical Operators",
+                    6: "Logical Operations in Conditions"
                 }
-                }
+                },
+            4:{"title": "Loops",
+               "description": "Learn how to use loops in order to improve your code.",
+               "questions": {
+                    1: "For with Range",
+                    2: "For with Lists",
+                    3: "While loop",
+                    4: "For loop with steps",
+                    5: "Break",
+                    6: "Continue"
+               }
+            }
+
            }
 
 
@@ -171,7 +184,7 @@ def q_1_5():
         evaluate_answer(answer)
 
 def q_2_1():
-        st.subheader("2.1: Intengers")
+        st.subheader(f"2.1: {chapters[2]['questions'][1]}")
         st.write("Integers are whole numbers. They can be positive, negative or even zero.")
         st.write("You can set a variable as int. For example:")
         st.write("These are some intergers.")
@@ -194,7 +207,7 @@ def q_2_1():
         evaluate_answer(answer)
 
 def q_2_2():
-        st.subheader("2.2: Floats")
+        st.subheader(f"2.2: {chapters[2]['questions'][2]}")
         st.write("Floats are numbers that are positive or negative, where they contain one or more decimals")
         st.write("You can set a variable as float. For example:")
         st.write("These are some floats.")
@@ -218,7 +231,7 @@ def q_2_2():
 
 
 def q_2_3():
-        st.subheader("2.3: Strings")
+        st.subheader(f"2.3: {chapters[2]['questions'][3]}")
         st.write("Strings are ways to use textual data. You can save a string in python through (\"\") or ('')")
         st.write("You can set variables as string. For example:")
         st.write("These are some strings.")
@@ -241,7 +254,7 @@ def q_2_3():
         evaluate_answer(answer)
 
 def q_2_4():
-        st.subheader("2.4: String Concatenation")
+        st.subheader(f"2.4: {chapters[2]['questions'][4]}")
         st.write("In Python we can concatenate strings using the + operator. For example:")
         st.write("Two strings are joined to create a message.")
 
@@ -257,7 +270,7 @@ def q_2_4():
         evaluate_answer(answer)
 
 def q_2_5():
-        st.subheader("2.5: Booleans")
+        st.subheader(f"2.5: {chapters[2]['questions'][5]}")
         st.write("Booleans can have only one of two values. They are either True or False. For example:")
         st.write("You can set one variable as False")
 
@@ -275,7 +288,7 @@ def q_2_5():
 
 
 def q_3_1():
-        st.subheader("3.1: Basic Comparisons")
+        st.subheader(f"3.1: {chapters[3]['questions'][1]}")
         st.write("In Python we can compare values ​​using operators such as == (equal), != (not equal), > (greater than), < (less than).")
         st.write("The outcome is either True or False. For example:")
         st.write("This is a true statement:")
@@ -297,7 +310,7 @@ def q_3_1():
 
 
 def q_3_2():
-        st.subheader("3.2: If Statement")
+        st.subheader(f"3.2: {chapters[3]['questions'][2]}")
         st.write("The if statement checks if it's true. If it's true it proceeds to execute the code. For example:")
         st.write("You can set one variable as False")
 
@@ -314,17 +327,194 @@ def q_3_2():
         evaluate_answer(answer)
 
 def q_3_3():
-        st.subheader("3.3: Logical Operators")
+        st.subheader(f"3.3: {chapters[3]['questions'][3]}")
+        st.write("The else statement goes with if statements. When an if statement is false then it will proceed to execute the else code. For example:")
+        st.write("This is an else statement")
+
+        col1, col2 = st.columns([0.6, 0.4])
+        with col1: st.code("num=-6\nif num > 0:\n     print(\"positive\")\nelse:\n     print(\"negative\")")
+        with col2: st.code("negative", language=None)
+
+        answer = "the name is leon"
+        st.session_state.editor_content = "name = \"leon\"\n"+\
+                                          "if        :\n"+\
+                                          "     print(\"the name is not leon\")\n"+\
+                                          "else:\n"+\
+                                          "     print()"
+        st.write(f"Make the if statement in order to print \"{answer}\"")
+        evaluate_answer(answer)       
+
+def q_3_4():
+        st.subheader(f"3.4: {chapters[3]['questions'][4]}")
+        st.write("The elif (else if) statement allows for testing multiple conditions. For example:")
+        st.write("This is an elif statement.")
+
+        col1, col2 = st.columns([0.6, 0.4])
+        with col1: st.code("num = 0\nif num > 0:\n     print(\"positive\")\nelif num == 0:\n     print(\"zero\")\nelse:\n     print(\"negative\")")
+        with col2: st.code("zero", language=None)
+
+        answer = "Equal to ten"
+        st.session_state.editor_content = "num = 10\n"+\
+                                          "if        :\n"+\
+                                          "     print(\"Bigger than ten\")\n"+\
+                                          "elif        :\n"+\
+                                          "     print(\"Equal to ten\")\n"+\
+                                          "else:\n"+\
+                                          "     print(\"Bigger than ten\")"
+        st.write(f"Complete the code in order to print \"{answer}\"")
+        evaluate_answer(answer)
+
+def q_3_5():
+        st.subheader(f"3.5: {chapters[3]['questions'][5]}")
         st.write("There three important logical operators. They are AND, OR and NOT.")
-        st.write("The operator AND is for when both conditions must be true. For Example:")
+        st.write("The operator AND returns true if both parts are true.")
         st.write("When one is true and the other is false, it becomes false. For Example:")
         col1, col2 = st.columns([0.6, 0.4])
-        with col1: st.code("greeting = \"Hey\"\nname=\"Friend\"\nprint(greeting + name))")
-        with col2: st.code("Hey Friend", language=None)
+        with col1: st.code("a, b = True, False\nif a and b:\n    print(\"This will not be printed\")#it will print nothing")
+        with col2: st.code("               ", language=None)
+        st.write("But when are both true, it becomes true. For Example:")
+        with col1: st.code("a, b = True, True\nif a and b:\n    print(\"This will be printed\")#it will print")
+        with col2: st.code("This will be printed", language=None)
 
-        answer = "minecraft"
-        st.session_state.editor_content = "animals = [\"cat\",\"dog\",\"snake\"]"
-        st.session_state.editor_content = "part1=\"mine\"\n"+\
-                    "part2=\"craft\"\n"
-        st.write(f"Combine part1 and part2 in order to print\"{answer}\"")
+        st.write("The operator OR returns true if either parts are true.")
+        st.write("When both are False, it becomes false. For Example:")
+        col1, col2 = st.columns([0.6, 0.4])
+        with col1: st.code("a, b = False, False\nif a or b:\n    print(\"This will not be printed\")#it will print nothing")
+        with col2: st.code("               ", language=None)
+        st.write("But when one of them is True, it becomes true. For Example:")
+        with col1: st.code("a, b = True, False\nif a or b:\n    print(\"This will be printed\")#it will print")
+        with col2: st.code("This will be printed", language=None)
+
+        st.write("The operator NOT reserves the condition.")
+        st.write("If the operant is true, when you put a not it will be false. For Example:")
+        col1, col2 = st.columns([0.6, 0.4])
+        with col1: st.code("a = True\nprint(not a):\n")
+        with col2: st.code("False", language=None)
+
+        answer = "Both are true\nOne of them is true\nThe not of one is equal to the normal state of the other"
+        st.session_state.editor_content = "s1 = True\n"+\
+                                          "s2 = False\n"+\
+                                          "s3 = True\n"+\
+                                          "if   and :\n"+\
+                                          "     print(\"Both are true\")\n"+\
+                                          "if   or :\n"+\
+                                          "     print(\"One of them is true\")\n"+\
+                                          "if not  ==  :\n"+\
+                                          "     print(\"The not of one is equal to the normal state of the other\")\n"
+        st.write("Complete the if statement in order to print these")
+        evaluate_answer(answer)
+
+def q_3_6():
+        st.subheader(f"3.6: {chapters[3]['questions'][6]}")
+        st.write("You can use logical operations in order to check conditions.")
+        st.write("They are many ways you can use and in if statements. For example:")
+        col1, col2 = st.columns([0.6, 0.4])
+        with col1: st.code("age = 14\nheight = 180\nif age==14 and height==180:\n    print(\"The person is 14 and has height 180 cm\")")
+        with col2: st.code("The person is 14 and has height 180 cm", language=None)
+        st.write("The same is true about or. For Example:")
+        with col1: st.code("age = 14\nheight = 180\nif age==14 or height==180:\n    print(\"The person is either 14 or has height 180 cm\")")
+        with col2: st.code("The person is either 14 or has height 180 cm", language=None)
+
+        answer = "Both are true\nOne of them is true\nThe not of one is equal to the normal state of the other"
+        st.session_state.editor_content = "s1 = True\n"+\
+                                          "s2 = False\n"+\
+                                          "s3 = True\n"+\
+                                          "if   and :\n"+\
+                                          "     print(\"Both are true\")\n"+\
+                                          "if   or :\n"+\
+                                          "     print(\"One of them is true\")\n"+\
+                                          "if not  ==  :\n"+\
+                                          "     print(\"The not of one is equal to the normal state of the other\")\n"
+        st.write("Complete the if statement in order to print these")
+        evaluate_answer(answer)
+
+def q_4_1():
+        st.subheader(f"4.1: {chapters[4]['questions'][1]}")
+        st.write("This loop repeats the code a certain number of times the user has chose.")
+        st.write("The range() function is used to choose the number of times it will repeat. For example:")
+        col1, col2 = st.columns([0.6, 0.4])
+        with col1: st.code("for i in range(5):\n    print(i)#it will print five times")
+        with col2: st.code("0\n1\n2\n3\n4", language=None)
+
+        answer = "0\n1\n2\n3"
+        st.session_state.editor_content = "for i in range():\n"+\
+                                          "    print()\n"
+        st.write("Complete the loop in order to print the numbers 0,1,2,3 in order.")
+        evaluate_answer(answer)
+
+def q_4_2():
+        st.subheader(f"4.2: {chapters[4]['questions'][2]}")
+        st.write("The for loop can iterate over the elements of a list.")
+        st.write("It is element per elements. For example:")
+        col1, col2 = st.columns([0.6, 0.4])
+        with col1: st.code("fruits = [\"apple\", \"banana\", \"orange\"]\nfor fruit in fruits:\n    print(fruit)#it will print the fruits")
+        with col2: st.code("apple\nbanana\norange", language=None)
+
+        answer = "cat\ndog\nturtle"
+        st.session_state.editor_content = "animals = []\n"+\
+                                          "for animal in :\n"+\
+                                          "    print()"
+        st.write("Complete the loop in order to print the animals cat, dog and turtle in order.")
+        evaluate_answer(answer)
+
+def q_4_3():
+        st.subheader(f"4.3: {chapters[4]['questions'][3]}")
+        st.write("The while loop repeats as long as a condition is true.")
+        st.write("You can count how many times it repeats. For example:")
+        col1, col2 = st.columns([0.6, 0.4])
+        with col1: st.code("count = 1\nwhile count <= 3:\n    print(count)  # Prints 1,2,3\n    count += 1")
+        with col2: st.code("1\n2\n3", language=None)
+
+        answer = "0\n1\n2"
+        st.session_state.editor_content = "num = \n"+\
+                                          "while num <= :\n"+\
+                                          "    print()\n"+\
+                                          "    num += "
+        st.write("Complete the loop in order to print the numbers 0, 1 and 2.")
+        evaluate_answer(answer)
+
+def q_4_4():
+        st.subheader(f"4.4: {chapters[4]['questions'][4]}")
+        st.write("Inside the range() you can skip steps. The range (x,y,z) the x is from when we start, y is for where it ends and z is the step it will go.")
+        st.write("You can use it in order to print even numbers. For example:")
+        col1, col2 = st.columns([0.6, 0.4])
+        with col1: st.code("for i in range(2, 7, 2):\n    print(i)")
+        with col2: st.code("2\n4\n6", language=None)
+
+        answer = "3\n6\n9"
+        st.session_state.editor_content = "for i in range(, , ):\n"+\
+                                          "    print()"
+        st.write("Complete the loop in order to print the numbers 3, 6 and 9.")
+        evaluate_answer(answer)
+
+def q_4_5():
+        st.subheader(f"4.5: {chapters[4]['questions'][5]}")
+        st.write("The break statement terminates a loop before it completes.")
+        st.write("You can combine it with an if statement. For example:")
+        col1, col2 = st.columns([0.6, 0.4])
+        with col1: st.code("for i in range(5):\n    if i == 3:\n        break\n    print(i)")
+        with col2: st.code("0\n1\n2", language=None)
+
+        answer = "0\n1\n2\n3\n4"
+        st.session_state.editor_content = "for i in range(6):\n"+\
+                                          "    if :\n"+\
+                                          "        break\n"+\
+                                          "    print()"
+        st.write("Make the loop to print until it is 4.")
+        evaluate_answer(answer)
+
+def q_4_6():
+        st.subheader(f"4.6: {chapters[4]['questions'][6]}")
+        st.write("The continue statement skips the current iteration and continues with the next one.")
+        st.write("You can use it to skip a number. For example:")
+        col1, col2 = st.columns([0.6, 0.4])
+        with col1: st.code("for i in range(1, 5):\n    if i == 2:\n        continue\n    print(i)")
+        with col2: st.code("1\n3\n4", language=None)
+
+        answer = "1\n2\n4"
+        st.session_state.editor_content = "for i in range(1, 5):\n"+\
+                                          "    if :\n"+\
+                                          "        continue\n"+\
+                                          "    print()"
+        st.write("Complete the code to skip the number 3 and print the remaining numbers from 1 to 4.")
         evaluate_answer(answer)
